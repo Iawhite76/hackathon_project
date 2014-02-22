74e5de9da0e12a9ee300bbf57ab34dc2cb85d6b2
Todd Wacker <twac05@yahoo.com>
2014-02-21 22:00:23 -0600

    Form now takes 3 items, Inventory has a for loop to add infinite items, Server has appropriate global variables

--- server.rb
+++ server.rb
@@ -21,7 +21,19 @@ post '/inventory' do
   @item = params[:item]
   @serial_number = params[:serial_number]
   @price = params[:price]
-  puts params
+  @pic = params[:pic]
+  @item2 = params[:item2]
+  @serial_number2 = params[:serial_number2]
+  @price2 = params[:price2]
+  @pic2 = params[:pic2]
+  @item3 = params[:item3]
+  @serial_number3 = params[:serial_number3]
+  @price3 = params[:price3]
+  @pic3 = params[:pic3]
+  @items = [@item, @item2, @item3]
+  @serial_numbers = [@serial_number, @serial_number2, @serial_number3]
+  @prices = [@price, @price2, @price3]
+  @pics = [@pic, @pic2, @pic3]
   erb :inventory
 end
 
