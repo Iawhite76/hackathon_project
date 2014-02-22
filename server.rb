1c9d206ab98c16d2d46d1d4367b322d24d965f75
Todd Wacker <twac05@yahoo.com>
2014-02-22 16:55:49 -0600

    Server updated, trying to allow photo to show on inventory page

--- server.rb
+++ server.rb
@@ -52,30 +52,32 @@ get '/inventory' do
   erb :inventory
 end
 
-# post '/inventory' do
-#   @name = params[:name]
-#   @user_name = params[:user_name]
-#   @phone = params[:phone]
-#   @email = params[:email]
-#   @zip = params[:zip]
-#   @item = params[:item]
-#   @serial_number = params[:serial_number]
-#   @price = params[:price]
-#   @pic = params[:pic]
-#   @item2 = params[:item2]
-#   @serial_number2 = params[:serial_number2]
-#   @price2 = params[:price2]
-#   @pic2 = params[:pic2]
-#   @item3 = params[:item3]
-#   @serial_number3 = params[:serial_number3]
-#   @price3 = params[:price3]
-#   @pic3 = params[:pic3]
-#   @items = [@item, @item2, @item3]
-#   @serial_numbers = [@serial_number, @serial_number2, @serial_number3]
-#   @prices = [@price, @price2, @price3]
-#   @pics = [@pic, @pic2, @pic3]
-#   erb :inventory
-# end
+post '/inventory' do
+  PHOTO_PATH=params[:pic][:tempfile]
+  @name = params[:name]
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @email = params[:email]
+  @zip = params[:zip]
+  @item = params[:item]
+  @serial_number = params[:serial_number]
+  @price = params[:price]
+  @pic = params[:pic]
+  @item2 = params[:item2]
+  @serial_number2 = params[:serial_number2]
+  @price2 = params[:price2]
+  @pic2 = params[:pic2]
+  @item3 = params[:item3]
+  @serial_number3 = params[:serial_number3]
+  @price3 = params[:price3]
+  @pic3 = params[:pic3]
+  @items = [@item, @item2, @item3]
+  @serial_numbers = [@serial_number, @serial_number2, @serial_number3]
+  @prices = [@price, @price2, @price3]
+  @pics = [@pic, @pic2, @pic3]
+  @id = flickr.upload_photo PHOTO_PATH, :title => @item, :description => 'serial number: ' + @serial_number + 'retail price: ' + @price, :tags => @hidden_name
+  erb :inventory
+end
 
 
 
